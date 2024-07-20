7f720a512446b7a250c0aba42cec4d4ef17bb04d
Mahesh Paul <123456magimagi@gmail.com>
2024-07-20 15:39:39 +0530

    styles not loading when route change fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,30 @@ import type { AppProps } from "next/app";
 import '@/styles/styles.scss';
 import Navbar from "@/components/Navbar";
 import { Raleway } from "@next/font/google";
+import Router from "next/router";
 
 const raleway = Raleway({
   subsets: ['latin'],
 })
 
 export default function App({ Component, pageProps, router }: AppProps) {
+  const routeChange = () => {
+    // Temporary fix to avoid flash of unstyled content
+    // during route transitions. Keep an eye on this
+    // issue and remove this code when resolved:
+    // https://github.com/vercel/next.js/issues/17464
+
+    const tempFix = () => {
+      const allStyleElems = document.querySelectorAll('style[media="x"]');
+      allStyleElems.forEach((elem) => {
+        elem.removeAttribute("media");
+      });
+    };
+    tempFix();
+  };
+
+ Router.events.on("routeChangeComplete", routeChange );
+ Router.events.on("routeChangeStart", routeChange );
   return (
     <main className={raleway.className}>
       <Navbar />
